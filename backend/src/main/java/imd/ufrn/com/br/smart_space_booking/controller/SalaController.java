2e820427a160a41c3f8c14e01843c9017b8d0d41
Luigi Peixoto <luigisp08@gmail.com>
2026-04-07 01:11:44 -0300

    feat: cria POST de reservas

---
+++ backend/src/main/java/imd/ufrn/com/br/smart_space_booking/controller/SalaController.java
@@ -0,0 +1,59 @@
+package imd.ufrn.com.br.smart_space_booking.controller;
+
+
+import imd.ufrn.com.br.smart_space_booking.dto.SalaResponseDTO;
+import imd.ufrn.com.br.smart_space_booking.model.Sala;
+import imd.ufrn.com.br.smart_space_booking.service.SalaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/salas")
+public class SalaController {
+
+    private final SalaService salaService;
+
+    public SalaController(SalaService salaService) {
+        this.salaService = salaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SalaResponseDTO>> listarTodas() {
+        List<SalaResponseDTO> salas = salaService.listarTodas();
+        return ResponseEntity.ok(salas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SalaResponseDTO> buscarPorId(@PathVariable Long id) {
+        return salaService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<SalaResponseDTO> criar(@RequestBody Sala sala) {
+        SalaResponseDTO response = salaService.salvar(sala);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id) {
+        boolean excluido = salaService.deletar(id);
+
+        if (excluido) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SalaResponseDTO> atualizar(@PathVariable Long id, @RequestBody Sala sala) {
+        SalaResponseDTO response = salaService.atualizar(id, sala);
+        return ResponseEntity.ok(response);
+    }
+
+}
---
+++ backend/src/main/java/imd/ufrn/com/br/smart_space_booking/service/SalaService.java
@@ -0,0 +1,70 @@
+package imd.ufrn.com.br.smart_space_booking.service;
+
+import imd.ufrn.com.br.smart_space_booking.dto.SalaResponseDTO;
+import imd.ufrn.com.br.smart_space_booking.model.Sala;
+import imd.ufrn.com.br.smart_space_booking.repository.SalaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SalaService {
+
+    private final SalaRepository salaRepository;
+
+    public SalaService(SalaRepository salaRepository) {
+        this.salaRepository = salaRepository;
+    }
+
+    public List<SalaResponseDTO> listarTodas() {
+        return salaRepository.findAll()
+                .stream()
+                .map(this::convertToDTO)
+                .toList();
+    }
+
+    public Optional<SalaResponseDTO> buscarPorId(Long id) {
+        return salaRepository.findById(id)
+                .map(this::convertToDTO);
+    }
+
+    public SalaResponseDTO salvar(Sala sala) {
+        Sala salaSalva = salaRepository.save(sala);
+        return convertToDTO(salaSalva);
+    }
+
+    public SalaResponseDTO atualizar(Long id, Sala salaDadosNovos) {
+        return salaRepository.findById(id).map(salaExistente -> {
+
+            salaExistente.setNome(salaDadosNovos.getNome());
+            salaExistente.setCapacidade(salaDadosNovos.getCapacidade());
+            salaExistente.setLocal(salaDadosNovos.getLocal());
+            salaExistente.setStatus(salaDadosNovos.getStatus());
+            salaExistente.setTipoSala(salaDadosNovos.getTipoSala());
+            salaExistente.setCaracteristicas(salaDadosNovos.getCaracteristicas());
+            //salaExistente.setImagem(salaDadosNovos.getImagem());
+            Sala salaAtualizada = salaRepository.save(salaExistente);
+            return convertToDTO(salaAtualizada);
+
+        }).orElseThrow(() -> new RuntimeException("Sala com ID " + id + " não encontrada!"));
+    }
+
+    public boolean deletar(Long id) {
+        return salaRepository.findById(id).map(sala -> {
+            salaRepository.delete(sala);
+            return true;
+        }).orElse(false);
+    }
+
+    private SalaResponseDTO convertToDTO(Sala sala) {
+        return new SalaResponseDTO(
+                sala.getId(),
+                sala.getNome(),
+                sala.getLocal(),
+                sala.getCapacidade(),
+                sala.getTipoSala().toString(),
+                sala.getStatus().toString(),
+                sala.getCaracteristicas());
+    }
+}
